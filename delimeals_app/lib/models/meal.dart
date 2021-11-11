c562637ba2de5e0f6165ba29f6af6fd2203027da
muhammedrishadpe <rishadpe@gmail.com>
2021-11-10 19:55:28 -0500

    Displaying recipe items & Using network image

--- delimeals_app/lib/models/meal.dart
+++ delimeals_app/lib/models/meal.dart
@@ -16,9 +16,9 @@ class Meal {
   final String? imageUrl;
   final List<String>? ingredients;
   final List<String>? steps;
-  final int? duration;
-  final Complexity? complexity;
-  final Affordability? affordability;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
   final bool? isGlutenFree;
   final bool? isLactoseFree;
   final bool? isVegan;
@@ -29,9 +29,9 @@ class Meal {
       @required this.imageUrl,
       @required this.ingredients,
       @required this.steps,
-      @required this.duration,
-      @required this.complexity,
-      @required this.affordability,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
       @required this.isGlutenFree,
       @required this.isLactoseFree,
       @required this.isVegan,
