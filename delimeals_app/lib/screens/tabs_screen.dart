7b51fd8c4a6a57b1cb914512404458815c6070ba
muhammedrishadpe <rishadpe@gmail.com>
2021-11-14 00:22:14 -0500

    Bottom Navigation Bar

--- delimeals_app/lib/screens/tabs_screen.dart
+++ delimeals_app/lib/screens/tabs_screen.dart
@@ -11,36 +11,13 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      // initialIndex: 1,
-      child: Scaffold(
+    return  Scaffold(
         appBar: AppBar(
           title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
-          ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
-      ),
+        body: null,
+        bottomNavigationBar: BottomNavigationBar(backgroundColor: Theme.of(context).primaryColor,
+        items: [BottomNavigationBarItem(icon: )],
     );
   }
 }
